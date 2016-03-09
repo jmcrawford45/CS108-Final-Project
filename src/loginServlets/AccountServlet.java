e6351882d4554f1dca158dd9d1db16dc07211dff
jmcrawford45 <jared13@stanford.edu>
2016-03-08 23:52:58 -0800

    Update AccountServlet.java

--- src/loginServlets/AccountServlet.java
+++ src/loginServlets/AccountServlet.java
@@ -1,7 +1,7 @@
 package loginServlets;
 
 import java.io.IOException;
-import table.TableAbstraction;
+import tableabstraction.TableAbstraction;
 import java.sql.*;
 import java.util.Random;
 
@@ -11,8 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import listeners.Security;
-import main.User;
+import user.User;
 
 /**
  * Servlet implementation class AccountServlet
@@ -39,18 +38,19 @@ public class AccountServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String name = (String)request.getParameter("username");
+		String name = (String)request.getParameter("name");
 		String password = (String)request.getParameter("password");
 		Connection con = (Connection)request.getSession().getServletContext().getAttribute("connection");
 		Random rand = (Random)request.getSession().getServletContext().getAttribute("rand");
 		User user = TableAbstraction.getUser(name,con);
 		if(user != null){
-            request.getRequestDispatcher("InUse.jsp").forward(request, response);
+            request.getRequestDispatcher("AreadyExistsJSP.jsp").forward(request, response);
         } else{
         	String salt = Security.getSalt(rand);
         	String hashPassword = Security.getHashed(password, salt);
         	TableAbstraction.updateUser(name, new User(name, hashPassword, salt), con);
-        	request.getRequestDispatcher("Welcome.jsp").forward(request, response);
+        	request.getSession().setAttribute("user",TableAbstraction.getUser(name, con));
+        	request.getRequestDispatcher("HomePage.jsp").forward(request, response);
         }
 	}
 
