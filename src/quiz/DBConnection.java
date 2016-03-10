e6c3acc6b22f15baed4c798e344bdfbd571d7777
jamisonsearles <jsearles@stanford.edu>
2016-03-09 21:02:24 -0800

    stored connection for testing

--- src/quiz/DBConnection.java
+++ src/quiz/DBConnection.java
@@ -7,16 +7,20 @@ import java.sql.Statement;
 
 public class DBConnection {
 
+	public static final String MYSQL_USERNAME = "ccs108jared13";
+	public static final String MYSQL_PASSWORD = "2zVt6gGlm5GtBhMf";
+	public static final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
+	public static final String MYSQL_DATABASE_NAME = "c_cs108_jared13";
+	
 	public static Connection connect() {
 		Connection con = null;
 		try {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://" + 
-            listeners.ContextListener.MYSQL_DATABASE_SERVER, 
-            listeners.ContextListener.MYSQL_USERNAME , 
-            listeners.ContextListener.MYSQL_PASSWORD);
+            MYSQL_DATABASE_SERVER, MYSQL_USERNAME , MYSQL_PASSWORD);
             Statement stmt = con.createStatement();
-            stmt.executeQuery("USE " + listeners.ContextListener.MYSQL_DATABASE_NAME);
+            stmt.executeQuery("USE " + MYSQL_DATABASE_NAME);
+         
     		} catch (SQLException e) {
             e.printStackTrace();
             }
